cc576051cc14c2bfbe3e0fd12c4db07af8782984
Jon Keam <jpkeam@gmail.com>
2023-10-07 02:03:00 -0400

    combine rm, finish, and unfinish into same block

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lib import Database, Todo, Util
 from os import environ
 from datetime import datetime
 from typing import List
+from operator import methodcaller
 load_dotenv()
 
 def find_matching(db:Database, id:str, table_name:str) -> Todo|None:
@@ -27,36 +28,26 @@ def main(table_name:str|None=None) -> None:
                 Util.print_table(todos)
             case "exit" | "quit":
                 command = "quit"
-            case "rm":
+            case "rm" | "finish" | "unfinish":
                 found = find_matching(db, commands[1], table_name)
                 if found is not None:
-                    found.delete()
-                else:
-                    print(f"Unable to find with id {commands[1]}")
-            case "finish":
-                found = find_matching(db, commands[1], table_name)
-                if found is not None:
-                    found.finish()
-                else:
-                    print(f"Unable to find with id {commands[1]}")
-            case "unfinish":
-                found = find_matching(db, commands[1], table_name)
-                if found is not None:
-                    found.unfinish()
+                    if command == "rm":
+                        command = "delete"
+                    methodcaller(command)(found)
                 else:
                     print(f"Unable to find with id {commands[1]}")
             case "set":
                 id = commands[1]
                 found = find_matching(db, id, table_name)
+                if found is None:
+                    print(f"Unable to find with id {id}")
+                    continue
+
                 args = Util.parse_args(" ".join(commands[2:]))
                 due_date = args.get("due_date", None)
                 if due_date is None:
                     due_date = args.get("date", None)
                 task = args.get("task", None)
-                if found is None:
-                    print(f"Unable to find with id {id}")
-                    continue
-
                 if due_date is not None:
                     found.update_due_date_as_str(due_date)
                 if task is not None:
